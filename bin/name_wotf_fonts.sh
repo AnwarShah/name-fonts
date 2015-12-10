4ecc5d1e1a2dea1775d24f0dc64a17314c62617e
Mohammad Anwar Shah <MohammadAnwarShah@Gmail.com>
2015-12-10 23:48:22 +0600

    Initial commit with working script

---
+++ bin/name_wotf_fonts.sh
@@ -0,0 +1,13 @@
+# This script extract font name and rename fonts to wotf
+
+# read one line at a time with read
+
+while read -r line
+do
+    # scan file and create a name and style with space substituted by _
+	name=`fc-scan --format "%{postscriptname}" "$line" | tr ' ' '_'`
+    
+    echo "$line->$name.wotf"
+	mv "$line" "$name.wotf"
+done < <(find . -maxdepth 1 -type f)
+
