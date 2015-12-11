43828d32d0e6c328436c6f9fd20f184d797e42a3
Mohammad Anwar Shah <MohammadAnwarShah@Gmail.com>
2015-12-11 19:50:59 +0600

    add extension according to it's type(ttf, wotf)

--- bin/name_fonts.sh
+++ bin/name_fonts.sh
@@ -6,13 +6,19 @@
 while read -r line
 do
   # scan file and create a name and style with space substituted by _
-	type=`file -i "$line"`
-  if [[ $type =~ 'application/octet-stream' ]];
+	type=`file -b "$line" | cut -d , -f 1`
+  if [[ "$type" =~ 'Web Open Font Format' ]];
   then
     name=`fc-scan --format "%{postscriptname}" "$line" | tr ' ' '_'`
     
     echo "$line->$name.wotf"
     mv "$line" "$name.wotf"
+  elif [[ "$type" =~ 'TrueType font data' ]];
+  then
+    name=`fc-scan --format "%{postscriptname}" "$line" | tr ' ' '_'`
+    
+    echo "$line->$name.ttf"
+    mv "$line" "$name.ttf"
   fi
 done < <(find . -maxdepth 1 -type f)
 
